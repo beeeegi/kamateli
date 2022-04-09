3a1acb944ab0e3dcf00414defd4a249d5bdc598a
saur0nnn <102294103+saur0nnn@users.noreply.github.com>
2022-04-09 18:21:41 +0400

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,26 @@
+import random
+import time
+
+min = 1
+max = 6
+
+tavidan = "ki"
+
+while tavidan == "ki":
+    print("\n-> KAMATELI MIGORAVS SHORRRSSSS...\n")
+    time.sleep(1)
+    print("-> KAMATELI MOGORDA SHENTAN....\n")
+    time.sleep(1)
+    print("== PIRVELI KAMATELI: " + str(random.randint(min, max)) + "\n")
+    time.sleep(1)
+    print("== MEORE KAMATELI: " + str(random.randint(min, max)) + "\n")
+    time.sleep(1)
+
+    print("-> DAVAI NARDIC VITAMASHOT\n")
+    time.sleep(1)
+    print("-> miutite 'ki' tu kidev ginda tamashi\n")
+    time.sleep(1)
+    print("-> daachire Enter-s tu agar ginda tamashi\n")
+    time.sleep(1)
+
+    tavidan = input("-> GAVAGORO TAVIDAN KAMATELI JO? ")
